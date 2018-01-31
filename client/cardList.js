2317edec696df8b23844b7ecbc918f5204f8597b
hbvbot <bjviloria104@gmail.com>
2018-01-30 16:30:19 -0800

    fix stuff

--- client/cardList.js
+++ client/cardList.js
@@ -6,15 +6,8 @@ angular.module('app')
     },
     template:
     `
-    <div class="flip-container" ontouchstart="this.classList.toggle('hover');" ng-repeat="card in $ctrl.cards">
-      <div class="flipper">
-        <div class="front">
-          <img class="resize" ng-src="{{card[$ctrl.img]}}">
-        </div>
-        <div class="back">
-          <img class="resize" ng-src="http://wow.zamimg.com/images/hearthstone/backs/original/Card_Back_Default.png">
-        </div>
-      </div>
-    </div>
+    <span ng-repeat="card in $ctrl.cards">
+      <img class="resize" ng-src="{{card[$ctrl.img]}}">
+    </span>
     `
   })
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const parser = require('body-parser');
+const unirest = require('unirest');
+
+app.use(parser.json());
+app.use(parser.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/client'));
+
+app.post('/hs', (req, res) => {
+  console.log(req.body)
+  const className = req.body.name;
+  const cost = req.body.cost;
+  let url = `https://omgvamp-hearthstone-v1.p.mashape.com/cards/classes/${className}`;
+  if (cost) {
+    url = `https://omgvamp-hearthstone-v1.p.mashape.com/cards/classes/${className}?cost=${cost}`
+  }
+
+  unirest.get(url)
+  .header("X-Mashape-Key", "Ad9LEibOO2msh901n5efHO2ALivap1XFop2jsnS61Cn3OWH6Sf")
+  .end(function (result) {
+    res.send(result.body);
+  });
+});
+
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Port is ${port}`);
+});
